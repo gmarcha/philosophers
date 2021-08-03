efe5c908bede795ebc4db92195ab58b083a283ef
gmarcha <gmarcha@github.com>
2021-08-03 19:07:56 -0400

    update src: add read input function: error check in input

---
+++ philo/src/input_read.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_read.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/03 17:12:20 by gamarcha          #+#    #+#             */
+/*   Updated: 2021/08/03 17:12:37 by gamarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static int	handle_input_invalid(char *errmsg)
+{
+	printf("%s: %s\n", PROGRAM_NAME, errmsg);
+	return (-1);
+}
+
+static int	is_input_valid(char *input[], size_t input_size)
+{
+	while (input_size != 0)
+	{
+		input_size--;
+		if (ft_aredigits(input[input_size]) == 0)
+			return (handle_input_invalid("argument is not an integer"));
+		while (*input[input_size] == '0')
+			input[input_size]++;
+		if (ft_strlen(input[input_size]) > 10
+			|| ft_atol(input[input_size]) > 2147483647l)
+			return (handle_input_invalid("argument overflow"));
+	}
+	return ((int)ft_atol(input[input_size]));
+}
+
+int	read_input(char *input[], size_t input_size)
+{
+	if (input_size == 1)
+		return (handle_input_invalid("number of philosophers,"
+				"time to die, time to eat, time to sleep, [number of meals]"));
+	if (input_size < 5)
+		return (handle_input_invalid("not enough arguments"));
+	if (input_size > 6)
+		return (handle_input_invalid("too much arguments"));
+	return (is_input_valid(input + 1, input_size - 1));
+}
