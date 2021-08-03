87834e4b3d0d61c39d15326cdaf26155370de463
gmarcha <gmarcha@github.com>
2021-08-03 19:08:30 -0400

    update src: add parse input function: parsing in a structure array

---
+++ philo/src/input_parse.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_parse.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/03 17:07:29 by gamarcha          #+#    #+#             */
+/*   Updated: 2021/08/03 18:41:54 by gamarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+t_philo	*parse_input(char *input[], size_t nb_philo)
+{
+	t_philo			*args;
+	size_t			i;
+
+	args = (t_philo *)malloc(sizeof(t_philo) * nb_philo);
+	if (args == NULL)
+		return (NULL);
+	i = 0;
+	while (i < nb_philo)
+	{
+		args[i].nb_philo = nb_philo;
+		args[i].index_philo = i;
+		args[i].time_to_die = (size_t)ft_atol(input[1]);
+		args[i].time_to_eat = (size_t)ft_atol(input[2]);
+		args[i].time_to_sleep = (size_t)ft_atol(input[3]);
+		if (input[4] != NULL)
+			args[i].nb_meals = (int)ft_atol(input[4]);
+		else
+			args[i].nb_meals = -1;
+		i++;
+	}
+	return (args);
+}
