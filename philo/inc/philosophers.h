2e65a11563f7a751f469677bfb94e91801fe3524
gmarcha <gmarcha@github.com>
2021-08-03 19:06:37 -0400

    update header: add read input and parse input prototypes and description

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -3,18 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gamarcha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 14:13:17 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/07/02 14:18:38 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/08/03 18:53:51 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# include <unistd.h>
 # include <string.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+# define PROGRAM_NAME	"philosophers"
+
+typedef struct s_philo
+{
+	size_t			nb_philo;
+	size_t			index_philo;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	int				nb_meals;
+	pthread_mutex_t	*forks;
+}					t_philo;
+
+///  ///////////////////////////////////////  ///
+///  ///          INPUT READING          ///  ///
+///  ///////////////////////////////////////  ///
+
+int			read_input(char *input[], size_t input_size);
+/*	Input validity check:									*/
+/*	- input is a valid integer, and is integer overflowing.	*/
+
+///  ///////////////////////////////////////  ///
+///  ///          INPUT PARSING          ///  ///
+///  ///////////////////////////////////////  ///
+
+t_philo		*parse_input(char *input[], size_t nb_philo);
+/*	Create args, as structure array,  for pthread_create calls:			*/
+/*	- parsing parameters in a specific structure for each philosophers.	*/
+
+///  ///////////////////////////////  ///
+///  ///          UTILS          ///  ///
+///  ///////////////////////////////  ///
+
+size_t		ft_strlen(const char *s);
+long		ft_atol(const char *nptr);
+int			ft_aredigits(const char *str);
 
 #endif
