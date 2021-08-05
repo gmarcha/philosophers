1a7c7a762d1b3d1bdd3b2964c2c867b27534df0b
gmarcha <gmarcha@github.com>
2021-08-05 16:11:29 -0400

    update src: correction in philosopher death routine

--- philo/src/philo/philo_death.c
+++ philo/src/philo/philo_death.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 08:29:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/08/05 15:47:32 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/08/05 16:11:12 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*philo_death(void *args)
 	{
 		pthread_mutex_lock(((t_philo *)args)->death);
 		if (((t_philo *)args)->died == 0
-			&& ft_current_time() + 5 - ((t_philo *)args)->last_meal > ((t_philo *)args)->time_to_die)
+			&& ft_current_time() - ((t_philo *)args)->last_meal > ((t_philo *)args)->time_to_die)
 		{
 			printf("%lu died\n", ((t_philo *)args)->index_philo + 1);
 			i = -1;
