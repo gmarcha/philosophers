f68b1af707dda8a0755125c6b303d0e7da05de3b
gmarcha <gmarcha@github.com>
2021-08-05 16:03:52 -0400

    update header: add some prototypes

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmarcha <gmarcha@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 14:13:17 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/08/04 13:33:07 by gmarcha          ###   ########.fr       */
+/*   Updated: 2021/08/05 15:34:05 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,18 @@
 
 # define PROGRAM_NAME	"philosophers"
 
+typedef unsigned int	t_bool;
+
 typedef unsigned long	t_msec;
+
+typedef struct s_threads
+{
+	pthread_mutex_t	*death;
+	pthread_mutex_t	*forks;
+	pthread_t		*philos;
+	pthread_t		*medics;
+}						t_threads;
+
 
 typedef struct s_philo
 {
@@ -32,7 +43,12 @@ typedef struct s_philo
 	size_t			time_to_eat;
 	size_t			time_to_sleep;
 	int				nb_meals;
+	t_msec			last_meal;
+	t_bool			died;
+	pthread_mutex_t	*death;
 	pthread_mutex_t	*forks;
+	pthread_t		philos;
+	struct s_philo	*args;
 }						t_philo;
 
 ///  ///////////////////////////////////////  ///
@@ -40,16 +56,21 @@ typedef struct s_philo
 ///  ///////////////////////////////////////  ///
 
 int			read_input(char *input[], size_t input_size);
-/*	Input validity check:									*/
-/*	- input is a valid integer, and is integer overflowing.	*/
+//	Input validity check:
+//	- input is a valid integer, and is integer overflowing.
 
 ///  ///////////////////////////////////////  ///
 ///  ///          INPUT PARSING          ///  ///
 ///  ///////////////////////////////////////  ///
 
 t_philo		*parse_input(char *input[], size_t nb_philo);
-/*	Create args, as structure array,  for pthread_create calls:			*/
-/*	- parsing parameters in a specific structure for each philosophers.	*/
+//	Create args, as structure array, for pthread_create calls:
+//	- parsing parameters in a specific structure for each philosophers.
+
+
+
+void		*philo_routine(void *args);
+void		*philo_death(void *args);
 
 ///  ///////////////////////////////  ///
 ///  ///          UTILS          ///  ///
