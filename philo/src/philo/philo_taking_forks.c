923e7c8d3a5a4a3ae2f1a6153064d8f08865641a
gmarcha <gmarcha@github.com>
2021-08-06 06:22:31 -0400

    update src: major correction in taking forks

--- philo/src/philo/philo_taking_forks.c
+++ philo/src/philo/philo_taking_forks.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 05:50:58 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/08/06 06:20:45 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/08/06 06:22:11 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static int	take_fork(t_philo *args, size_t id_fork)
 	t_msec			timer;
 
 	pthread_mutex_lock(args->forks + id_fork);
-	printf("How ?\n");
 	ret = 1;
 	pthread_mutex_lock(args->death);
 	timer = ft_current_time() - args->begin_time;
@@ -40,10 +39,10 @@ int	philo_taking_forks(t_philo *args)
 		return (1);
 	}
 	printf("GO RIGHT: %lu\n", (args->id_philo + 1) % args->nb_philo);
-	if (take_fork(args, args->id_philo + 1 % args->nb_philo) == 1)
+	if (take_fork(args, (args->id_philo + 1) % args->nb_philo) == 1)
 	{
 		pthread_mutex_unlock(args->forks + args->id_philo);
-		pthread_mutex_unlock(args->forks + args->id_philo + 1 % args->nb_philo);
+		pthread_mutex_unlock(args->forks + ((args->id_philo + 1) % args->nb_philo));
 		return (1);
 	}
 	return (0);
